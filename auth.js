0de617b5b4a41a5ef8d1f8a9ab5212e1986127e4
wangyong <1146040444@qq.com>
2024-10-07 01:05:16 +0800

    --

--- auth.js
+++ auth.js
@@ -110,9 +110,6 @@ function authUser(uname, cgroup) {
             stdio: ['ignore', 'ignore', 'ignore', 'ipc']
           },
           monitor: true,
-          only: true,
-          onlyArgs: ['--uid', ${au.uid}],
-          force: true,
           callback: (ch, cm) => {
             ch.on('error', err => {
               cm.errorHandle(err, '--CHILD--')
